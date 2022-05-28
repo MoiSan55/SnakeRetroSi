012d6231b0d04c0da0d7aa0f83fe2cf3804002d4
MoiSan55 <105457468+MoiSan55@users.noreply.github.com>
2022-05-27 22:51:50 -0700

    Add files via upload

---
+++ snake game/Dificultad.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace snake_game
+{
+    class Dificultad
+    {
+        public int Min = 3, Seg = 0;
+        public int configuracionDificultad()
+        {
+            return 1;
+        }
+
+        public void Cronometro()
+        {
+            Min = 2;
+            Seg = 50;
+        }
+
+        public int ModoJuego()
+        {
+            return 2;
+        }
+
+    }
+}
